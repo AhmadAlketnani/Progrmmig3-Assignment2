e9a11c0e16da15d1b046dd951172a19bdbfa3bca
AhmadAlketnani <ahmadalktnany383@gmail.com>
2023-03-14 20:48:31 +0200

    Lecture11 files

--- src/Lecture11/MainApp.java
+++ src/Lecture11/MainApp.java
@@ -20,16 +20,20 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 /**
- *
  * @author aashgar
  */
-public class MainApp extends Application{
+public class MainApp extends Application {
     private Label labelTitle, labelError;
     private TextField textFieldLoginName;
     private PasswordField passwordField;
     private Button buttonSubmit, buttonCancel;
     private Stage stage;
+
     @Override
     public void start(Stage primaryStage) throws Exception {
         labelTitle = new Label("Login Information");
@@ -42,7 +46,7 @@ public class MainApp extends Application{
         VBox vBox1 = new VBox(10, labelTitle, textFieldLoginName, passwordField, labelError);
         vBox1.setAlignment(Pos.CENTER);
         vBox1.setStyle("-fx-border-color: red");
-        
+
         buttonSubmit = new Button("Submit");
         buttonCancel = new Button("Cancel");
         buttonSubmit.setOnAction(new MyEventHandler());
@@ -52,48 +56,70 @@ public class MainApp extends Application{
         hBox1.setSpacing(20);
         hBox1.setAlignment(Pos.CENTER);
         hBox1.setStyle("-fx-border-color: lime");
-        
+
         VBox vBox2 = new VBox(10, vBox1, hBox1);
         vBox2.setAlignment(Pos.CENTER);
         vBox2.setStyle("-fx-border-color: navy");
         vBox2.setPadding(new Insets(10, 20, 10, 20));
         vBox2.getStyleClass().add("vbox");
-        
+
         FlowPane flowPane = new FlowPane(vBox2);
         flowPane.setAlignment(Pos.CENTER);
-        
+
         Scene scene = new Scene(flowPane, 400, 300);
 //        scene.getStylesheets().add("file:src/Ch2Part2Apps2/styles.css");
         scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
-        primaryStage.setScene(scene);        
+        primaryStage.setScene(scene);
         primaryStage.setTitle("Login Screen");
         primaryStage.show();
         stage = primaryStage;
     }
-    
+
     public static void main(String[] args) {
         launch(args);
-    } 
-    private class MyEventHandler implements EventHandler<ActionEvent>{
+    }
+
+    private class MyEventHandler implements EventHandler<ActionEvent> {
 
         @Override
         public void handle(ActionEvent event) {
-            if(event.getSource() == buttonSubmit){
-                if(textFieldLoginName.getText().equals("a") &&
-                        passwordField.getText().equals("1")){
-                    labelError.setText("Valid User");
-                    stage.close();
-                    new MainScreen().show();
-                    
+            if (event.getSource() == buttonSubmit) {
+                File loginFile;
+                try {
+                    loginFile = new File("./src/Lecture11/password.txt");
+                    Scanner readerFile = new Scanner(loginFile);
+                    boolean notLogin = true;
+                    while (readerFile.hasNextLine()) {
+                        if (textFieldLoginName.getText().equals(readerFile.next()) &&
+                                passwordField.getText().equals(readerFile.next())
+
+                        ) {
+                            labelError.setText("Valid User");
+                            readerFile.close();
+                            stage.close();
+                            new MainScreen().show();
+                        }
+                    }
+                    if (notLogin) labelError.setText("Invalid Data");
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
                 }
-                else
-                    labelError.setText("Invalid Data");
-            }
-            else if (event.getSource() == buttonCancel){
+
+
+//                if(textFieldLoginName.getText().equals("a") &&
+//                        passwordField.getText().equals("1")){
+//                    labelError.setText("Valid User");
+//                    stage.close();
+//                    new MainScreen().show();
+//
+//                }
+//                else
+//                    labelError.setText("Invalid Data");
+            } else if (event.getSource() == buttonCancel) {
                 textFieldLoginName.setText("");
                 passwordField.setText("");
             }
         }
-        
+
     }
 }
--- src/Lecture11/MainScreen.java
+++ src/Lecture11/MainScreen.java
@@ -5,11 +5,11 @@
  */
 package Lecture11;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Menu;
@@ -25,22 +25,96 @@ import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 /**
- *
  * @author aashgar
  */
-public class MainScreen extends Stage{
+public class MainScreen extends Stage {
     private MenuBar menuBar;
-    private Menu menuFile;
-    private MenuItem menuItemOpen;
+    private Menu menuFile, menuColor, menuAbout;
+    private MenuItem menuItemOpen, menuItemClear, menuItemSave, menuItemExit,
+            menuItemGold, menuItemCyan, menuItemGrey, menuItemAbout;
     private TextArea textArea;
     private Slider sliderFontSize;
+    private File fileSelected;
+private String filePath;
     public MainScreen() {
         menuBar = new MenuBar();
+
         menuFile = new Menu("File");
+        menuColor = new Menu("Color");
+        menuAbout = new Menu("About");
+
         menuItemOpen = new MenuItem("Open");
-        menuFile.getItems().add(menuItemOpen);
-        menuBar.getMenus().add(menuFile);       
-        
+        menuItemOpen.setOnAction(e->{
+            FileChooser fileChooser=new FileChooser();
+            fileSelected=fileChooser.showOpenDialog(this);
+            filePath =fileSelected.getAbsolutePath();
+            try {
+                Scanner fileReader=new Scanner(fileSelected);
+                textArea.clear();
+                while (fileReader.hasNextLine()){
+                    textArea.appendText(fileReader.nextLine()+"\n");
+                }
+                fileReader.close();
+            } catch (FileNotFoundException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        menuItemClear = new MenuItem("Clear");
+        menuItemClear.setOnAction(e-> textArea.setText(""));
+
+        menuItemSave = new MenuItem("Save");
+        menuItemSave.setOnAction(e->{
+            try {
+                File file = new File(filePath);
+                FileWriter fileWriter = new FileWriter(file);
+                PrintWriter printWriter = new PrintWriter(fileWriter);
+                printWriter.print(textArea.getText());
+                fileWriter.close();
+                printWriter.close();
+            } catch (FileNotFoundException ex) {
+                throw new RuntimeException(ex);
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        menuItemExit = new MenuItem("Exit");
+        menuItemExit.setOnAction(e-> this.close());
+
+
+        menuItemGold = new MenuItem("Gold");
+        menuItemCyan = new MenuItem("Cyan");
+        menuItemGrey = new MenuItem("Grey");
+        // menu color handle
+        menuItemGold.setOnAction(e
+                -> {
+            textArea.setStyle("-fx-text-fill:gold;-fx-font-size: "
+                    + sliderFontSize.getValue() + "pt");
+        });
+        menuItemCyan.setOnAction(e
+                -> {
+            textArea.setStyle("-fx-text-fill:cyan;-fx-font-size: "
+                    + sliderFontSize.getValue() + "pt");
+        });
+        menuItemGrey.setOnAction(e
+                -> {
+            textArea.setStyle("-fx-text-fill:grey;-fx-font-size: "
+                    + sliderFontSize.getValue() + "pt");
+        });
+
+        menuItemAbout = new MenuItem("About");
+        menuItemAbout.setOnAction(event -> {
+            textArea.setText("Name: Ahmad Alketnani \nID : 120200317 \nAge : 20 \nI'm MERN Stack Developer");
+        });
+
+        menuFile.getItems().addAll(menuItemOpen, menuItemSave, menuItemClear, menuItemExit);
+        menuColor.getItems().addAll(menuItemGold, menuItemCyan, menuItemGrey);
+        menuAbout.getItems().addAll(menuItemAbout);
+
+        menuBar.getMenus().addAll(menuFile, menuColor, menuAbout);
+
+
         textArea = new TextArea("Playing with JavaFX Advanced Controls");
         textArea.setMaxHeight(300);
         textArea.setMaxWidth(500);
@@ -49,53 +123,42 @@ public class MainScreen extends Stage{
         sliderFontSize.setMinorTickCount(4);
         sliderFontSize.setShowTickLabels(true);
         sliderFontSize.setShowTickMarks(true);
-        sliderFontSize.setSnapToTicks(true);        
+        sliderFontSize.setSnapToTicks(true);
         ComboBox<String> comboBoxLinks = new ComboBox<>();
         comboBoxLinks.getItems().addAll("Instructor GitHub",
-                "JavaFX Guide");
+                "JavaFX Guide", "My GitHub");
         comboBoxLinks.getSelectionModel().select(1);
         WebView webView = new WebView();
-        
+
         HBox hBox1 = new HBox(15, comboBoxLinks, webView);
-        
+
         VBox vBox1 = new VBox(15, textArea, sliderFontSize,
-                hBox1);        
-        
+                hBox1);
+
         BorderPane borderPane = new BorderPane();
         borderPane.setTop(menuBar);
         borderPane.setCenter(vBox1);
-        
+
         Scene scene = new Scene(borderPane, 1000, 700);
         this.setScene(scene);
         this.setTitle("Advanced JavaFX Screen");
-        sliderFontSize.valueProperty().addListener(e->{
+        sliderFontSize.valueProperty().addListener(e -> {
             textArea.setStyle("-fx-font-size: " +
-                    sliderFontSize.getValue()+"pt");
+                    sliderFontSize.getValue() + "pt");
         });
-        comboBoxLinks.setOnAction(e->{
-        if(comboBoxLinks.getSelectionModel()
-                .getSelectedIndex()== 0)
-            webView.getEngine()
-                    .load("https://github.com/aashgar");
-        
-        });
-        menuItemOpen.setOnAction(e->{
-            FileChooser fileChooser = new FileChooser();
-            fileChooser.setInitialDirectory
-                        (new File("src/Ch2Part2Apps2"));
-            File selectedFile= fileChooser.showOpenDialog(this);
-            
-            try {
-                Scanner s=new Scanner(selectedFile);
-                while(s.hasNextLine()){
-                    textArea.appendText(s.nextLine()+"\n");                   
-                }
-                s.close();
-            } catch (FileNotFoundException ex) {
-                Logger.getLogger(MainScreen.class.getName()).log(Level.SEVERE, null, ex);
+        comboBoxLinks.setOnAction(e -> {
+            if (comboBoxLinks.getSelectionModel()
+                    .getSelectedIndex() == 0) {
+                webView.getEngine()
+                        .load("https://github.com/aashgar");
+            } else if (comboBoxLinks.getSelectionModel()
+                    .getSelectedIndex() == 1) {
+                webView.getEngine().load("https://openjfx.io/openjfx-docs/");
+            } else {
+                webView.getEngine().load("https://github.com/AhmadAlketnani");
             }
-        
         });
+
     }
-    
+
 }
